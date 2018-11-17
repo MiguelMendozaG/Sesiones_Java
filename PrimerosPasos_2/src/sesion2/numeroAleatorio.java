e182d20fd0c45e6d77f50fc36def55bf001d02ab
miguel <miguel.mendoza.11.1987@gmail.com>
2018-11-16 21:57:20 -0600

    sesion2_b

--- PrimerosPasos_2/src/sesion2/numeroAleatorio.java
+++ PrimerosPasos_2/src/sesion2/numeroAleatorio.java
@@ -5,10 +5,36 @@
  */
 package sesion2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author miguel
  */
 public class numeroAleatorio {
+    public static void main(String[] args) {
+        
+        int aleatorio = (int) (Math.random() * 100);
+        int numero = 0;
+        
+        int intentos = 0;
+        
+        while( aleatorio != numero){
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero"));
+            
+            if (aleatorio > numero){
+                System.out.println("Te falta mas");
+            }
+            
+            else if ( aleatorio < numero){
+                System.out.println("Te pasaste");
+            }
+            
+            intentos++;
+        }
+        
+        System.out.println("Listo adivinaste el numero fue " + numero);
+        System.out.println("Y fue en " + intentos);
+    }
     
 }
